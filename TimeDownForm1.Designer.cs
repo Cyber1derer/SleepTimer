0db752f03e5f19cab417865ace8d388b61f41699
Denis V <Vorkutov.Denis.A@yandex.ru>
2020-11-05 05:04:22 +0300

    First upload project

---
+++ TimeDownForm1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class TimeDownForm1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeDownForm1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Down = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHour = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textMinut = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.Loop = new System.Windows.Forms.Button();
+            this.Keyboard = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(136, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(390, 79);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запустить таймер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(136, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(390, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Down
+            // 
+            this.Down.Interval = 1000;
+            this.Down.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(42, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 64);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Осталось";
+            // 
+            // textHour
+            // 
+            this.textHour.Location = new System.Drawing.Point(245, 42);
+            this.textHour.Name = "textHour";
+            this.textHour.Size = new System.Drawing.Size(57, 20);
+            this.textHour.TabIndex = 5;
+            this.textHour.Text = "0";
+            this.textHour.TextChanged += new System.EventHandler(this.textHour_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(49, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Выключить через";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(308, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ч.   :";
+            // 
+            // textMinut
+            // 
+            this.textMinut.Location = new System.Drawing.Point(378, 42);
+            this.textMinut.Name = "textMinut";
+            this.textMinut.Size = new System.Drawing.Size(63, 20);
+            this.textMinut.TabIndex = 8;
+            this.textMinut.Text = "0";
+            this.textMinut.TextChanged += new System.EventHandler(this.textMinut_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(447, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "мин.";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(378, 66);
+            this.trackBar1.Maximum = 60;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(223, 66);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Loop
+            // 
+            this.Loop.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Loop.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Loop.Location = new System.Drawing.Point(13, 213);
+            this.Loop.Name = "Loop";
+            this.Loop.Size = new System.Drawing.Size(91, 60);
+            this.Loop.TabIndex = 11;
+            this.Loop.Text = "Лупа";
+            this.Loop.UseMnemonic = false;
+            this.Loop.UseVisualStyleBackColor = false;
+            this.Loop.Click += new System.EventHandler(this.Loop_Click);
+            // 
+            // Keyboard
+            // 
+            this.Keyboard.BackColor = System.Drawing.Color.Goldenrod;
+            this.Keyboard.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Keyboard.Location = new System.Drawing.Point(13, 279);
+            this.Keyboard.Name = "Keyboard";
+            this.Keyboard.Size = new System.Drawing.Size(91, 60);
+            this.Keyboard.TabIndex = 12;
+            this.Keyboard.Text = "Пупа";
+            this.Keyboard.UseVisualStyleBackColor = false;
+            this.Keyboard.Click += new System.EventHandler(this.Keyboard_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(650, 406);
+            this.Controls.Add(this.Keyboard);
+            this.Controls.Add(this.Loop);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textMinut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textHour);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Таймер выключения";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Timer Down;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHour;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textMinut;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button Loop;
+        private System.Windows.Forms.Button Keyboard;
+    }
+}
+
---
+++ TimeDownForm1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class TimeDownForm1 : Form
+    {
+        int time = 60 * 25;
+        bool pbar = true;
+        double MaxTime;
+        public TimeDownForm1()
+        {
+            InitializeComponent();
+            label1.Text = ("Осталось     " + time / 60 + ":" + (time % 60));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            //Делаем таймер доступным
+            Down.Enabled = true;
+            //Запускаем таймер
+            Down.Start();
+            button1.Visible = false;
+            button2.Visible = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Down.Stop();
+            time = Int32.Parse(textHour.Text) * 3600 + Int32.Parse(textMinut.Text) * 60;
+            pbar = true;
+            button1.Visible = true;
+            button2.Visible = false;
+        }
+
+     private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pbar) {
+                MaxTime = time/100;
+                pbar = false;
+            }
+            time--;
+            label1.Text = ("Осталось     " + time/60 + ":"+ (time % 60));
+            if (time <=0)
+            {
+                Process.Start("shutdown", "-s -f -t 0");
+            }
+        }
+        private void textHour_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                time = Int32.Parse(textHour.Text) * 360 + Int32.Parse(textMinut.Text) * 60;
+            }
+            catch
+            {
+                time =Int32.Parse(textMinut.Text) * 60;
+                textHour.Text = "0";
+            }
+        }
+
+
+        private void textMinut_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                time = Int32.Parse(textHour.Text) * 3600 + Int32.Parse(textMinut.Text) * 60;
+            }
+            catch
+            {
+                time = Int32.Parse(textHour.Text) * 3600 + 0;
+                textMinut.Text ="0";
+            }
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textMinut.Text = (trackBar1.Value).ToString();
+            time = Int32.Parse(textHour.Text)*3600 + trackBar1.Value*60;
+            label1.Text = ("Осталось     " + time / 60 + ":" + (time % 60));
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            textHour.Text = (trackBar2.Value).ToString();
+            time = trackBar2.Value*3600 + Int32.Parse(textMinut.Text)*60;
+            label1.Text = ("Осталось     " + time / 60 + ":" + (time % 60));
+        }
+
+        private void Loop_Click(object sender, EventArgs e)
+        {
+            Process.Start("Magnify.exe");
+        }
+
+
+    }
+}
